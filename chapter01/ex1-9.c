4a5bb53e1ccdd2e3747aea4c70fbad01f74bb896
Vyshak N <vyshak.n.93@gmail.com>
2015-06-15 14:56:06 +0530

    Initial Commit - 1.5

---
+++ chapter01/ex1-9.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+#define SET 1
+#define UNSET 0
+
+/* replaces multiple blanks with single ones */
+
+int main()
+{
+
+	int c,state;
+
+	state = UNSET;
+
+	while((c = getchar()) != EOF ) {
+		if (c == ' ') {
+			if(state == UNSET) {
+				state = SET;
+				putchar(c);
+			}
+		} else if (state == SET) {
+			state = UNSET;
+			putchar(c);
+		} else
+			putchar(c);			
+	}
+
+	return 0;
+}
